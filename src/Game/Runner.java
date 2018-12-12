5730c2826ea42f0406fda599848b382dd3ffbad8
bryanttsang <43279760+bryanttsang@users.noreply.github.com>
2018-12-12 16:42:05 -0500

    Update Runner.java

--- src/Game/Runner.java
+++ src/Game/Runner.java
@@ -8,8 +8,6 @@ import Board.Board;
 
 import java.util.Scanner;
 
-import static Board.Board.board;
-
 public class Runner {
 
 	public static Room[][] building = new Room[5][5];
@@ -18,7 +16,6 @@ public class Runner {
 	
 	public static void main(String[] args)
 	{
-		
 		//Fill the building with normal rooms
 		for (int x = 0; x<building.length; x++)
 		{
@@ -48,26 +45,29 @@ public class Runner {
 
 		Board Board = new Board(building);
 
-
 		 //Setup player 1 and the input scanner
 		Person player1 = new Person("FirstName", "FamilyName", 0,0);
 		building[0][0].enterRoom(player1);
 		Scanner in = new Scanner(System.in);
 		while(gameOn)
 		{
+			if (player1.getxLoc() == x && player1.getyLoc() == y)
+			{
+				System.out.println("You found the winning room! Ten points for Gryffindor.");
+				Runner.gameOff();
+				break;
+			}
 			Board.print();
 			System.out.println("Where would you like to move? (Choose N, S, E, W)");
 			String move = in.nextLine();
 			if(validMove(move, player1, building))
 			{
 				System.out.println("Your coordinates: row = " + player1.getxLoc() + " col = " + player1.getyLoc());
-				
 			}
-			else {
+			else 
+			{
 				System.out.println("Please choose a valid move.");
 			}
-			
-			
 		}
 		in.close();
 	}
@@ -131,10 +131,10 @@ public class Runner {
 				}
 			default:
 				break;
-					
 		}
 		return true;
 	}
+	
 	public static void gameOff()
 	{
 		gameOn = false;
@@ -145,5 +145,4 @@ public class Runner {
 		return (int)(Math.random()*building.length);
 
 	}
-
 }
